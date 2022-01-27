2c2f7ecb77216199d6d98f2036ac541b4a2d7d05
Ben Partington <bdpartington@gmail.com>
2022-01-27 09:28:16 +0000

    Section 10 Refactor

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,8 @@
 class DockingStation
   def release_bike
-    
   end
 
   def dock(bike)
-
   end 
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,23 +2,13 @@ require 'docking_station'
 
 describe DockingStation do 
   describe "#release_bike" do 
-    it 'should repsond to release bike method' do
-      docking_station = DockingStation.new
-      expect(docking_station).to respond_to(:release_bike)
-    end  
-  end 
-  
-  describe "#release_bike" do 
     it 'should release a bike' do 
-      docking_station = DockingStation.new
       bike = Bike.new 
-      expect(docking_station.release_bike).to eq(bike.working?)
+      expect(subject.release_bike).to eq(bike.working?)
     end 
   end 
 
+  it { is_expected.to respond_to :release_bike }
+
   it { is_expected.to respond_to(:dock).with(1).argument }
-end
-
-
-#it { is_expected.to respond_to :release_bike }
-
+end 
