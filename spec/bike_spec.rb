5a086fba96065f468c523ef528d51591e23d411c
meghanblyth <megblyth@gmail.com>
2022-01-27 15:29:47 +0000

    Complete raising exceptions - Added release bike capacity

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/docking_station.rb'
+require_relative '../lib/bike.rb'
 
 describe Bike do
   it { is_expected.to respond_to :working? }
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,34 @@
 require 'docking_station'
 
 describe DockingStation do 
+
   describe "#release_bike" do 
     it 'should release a bike' do 
       bike = Bike.new 
-      expect(subject.release_bike).to eq(bike.working?)
+      subject.dock(bike) 
+      expect(subject.release_bike).to eq bike 
     end 
+
+    it "raises an error when there are no bikes available" do 
+      expect{subject.release_bike}.to raise_error "No bikes available" 
+    end 
+
+    it { is_expected.to respond_to :release_bike }
+  end
+
+  it 'docks something' do 
+    bike = Bike.new 
+    expect(subject.dock(bike)).to eq bike 
   end 
 
-  it { is_expected.to respond_to :release_bike }
+  it 'returns docked bikes' do 
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike 
+  end 
 
   it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) } 
+
 end 
